cef348ea7b5839dcc3a568b3353bbd474a0c4f04
rohit-ash-choudhary <71401237+rohit-ash-choudhary@users.noreply.github.com>
2022-07-02 05:54:42 +0530

    Add files via upload
    Star Patterns

---
+++ src/July2_2022/StarPattern.java
@@ -0,0 +1,17 @@
+package June22_2022;
+import java.util.*;
+public class StarPattern {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number");
+        int num=sc.nextInt();
+        for (int i = 0; i < num; i++) {
+            for (int j = 0; j < num-i; j++) {
+                System.out.print ("* ");
+
+            }
+            System.out.println();
+        }
+
+    }
+}
